41613a3a2d0674e058eb25e9bded8d248039f888
Aryan <as1991218113@gmail.com>
2026-01-25 18:38:39 +0530

    comment

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -16,7 +16,7 @@ export default function AdminHomePage() {
         <Hero />
         <AboutSection />
         <Teaser />
-        <EventsSection />
+        {/* <EventsSection /> */}
         <Highlights />
       </div>
       <Footer />
